0de34645d51f2a9c32e0c5f1a86428dc25752f84
Luis1505 <luis.montanez-vazquez@patelhs.org>
2020-12-07 17:40:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+  public static void main(String[] args) {
+    Sum s = new Sum(12,13);
+    System.out.println(s.sumOf());
+  }
+}
---
+++ sum.java
@@ -0,0 +1,17 @@
+public class Sum{
+  //attributes-- AKA variables
+  private int num1;
+  private int num2;
+
+ // Sum constructor
+ public Sum(int number1, int number2){
+   num1 = number1;
+   num2 = number2;
+ }
+ //behavior
+ //my sumOf method will be adding both variables
+  public int sumOf(){
+   return num1 + num2;
+    
+  }
+}
